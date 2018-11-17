367654c51eb6e5da03b02946820aa9cb82b5547c
jiedevstudio <jiedev@163.com>
2018-11-17 11:11:12 +0800

    add

--- webapp/Startup.cs
+++ webapp/Startup.cs
@@ -40,7 +40,7 @@ namespace webapp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IAppBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
